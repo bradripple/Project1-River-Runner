2f8dc8402f8b230ff21fc2951df3e1e697ba7647
bradripple <bradripple@gmail.com>
2021-08-16 10:48:11 -0600

    Creating guidelines in index.js and created basic html5 structure

---
+++ index.js
@@ -0,0 +1,21 @@
+// GLOBAL DOM / VARIABLES
+
+// ====================== SETUP FOR CANVAS RENDERING ======================= //
+// 2D rendering context for canvas element.
+// It is used for drawing shapes, text, images, and other objects.
+// Set your Context!
+
+// ====================== ENTITIES ======================= //
+
+// ====================== HELPER FUNCTIONS ======================= //
+// SANDBOX FOR TESTING PAINTING TECHNIQUES
+
+//  GUI
+
+//  KEYBOARD INTERACTION LOGIC
+
+// ====================== GAME PROCESSES ======================= //
+
+// ====================== COLLISION DETECTION ======================= //
+
+// ====================== PAINT INTIAL SCREEN ======================= //
